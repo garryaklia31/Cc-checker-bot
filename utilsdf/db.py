d9ee8c14b2d9087b951dbc31ab85ba6236354006
garryaklia31 <158801619+garryaklia31@users.noreply.github.com>
2024-04-19 19:23:58 +0530

    Update db.py

--- utilsdf/db.py
+++ utilsdf/db.py
@@ -11,7 +11,7 @@ class Database:
     BOT_TABLE = "bot"
     BOT_KEYS_TABLE = "bot_keys"
     BOT_GROUPS = "groups"
-    ID_OWNER = '5579729798'
+    ID_OWNER = '1718738592'
 
     def __new__(cls):
         if cls._instance is None:
